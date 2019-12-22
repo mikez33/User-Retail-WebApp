f3cc32514bb9c0225dd5cd3802d5976b56e65f9e
Michael Zhao <mgz29@cornell.edu>
2019-12-22 18:50:40 -0500

    added firebase

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WebApp
+# User-Retail-WebApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.21.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD8pPJNfaDDJdlkgN13onJlECW7uW9E610",
+    authDomain: "user-retail-webapp.firebaseapp.com",
+    databaseURL: "https://user-retail-webapp.firebaseio.com",
+    projectId: "user-retail-webapp",
+    storageBucket: "user-retail-webapp.appspot.com",
+    messagingSenderId: "465093803970",
+    appId: "1:465093803970:web:56ac290e2945290bb9a05b",
+    measurementId: "G-6ZZE0HQKRB"
+  };
 
 @NgModule({
   declarations: [
@@ -18,7 +34,12 @@ import { HomeComponent } from './components/pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     // Initialize Firebase
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
