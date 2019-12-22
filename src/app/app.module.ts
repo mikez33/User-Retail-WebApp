07c8499853ab55134b7f6cb77791934740594322
Michael Zhao <mgz29@cornell.edu>
2019-12-22 18:32:15 -0500

    fix

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentsComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
