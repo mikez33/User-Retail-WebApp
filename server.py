f022aa2ef89eb97894cd012d1dde2d0cd594ec9f
Michael Zhao <mzhao@anysphere.co>
2026-09-03 15:48:47 -0400

    next

--- server.py
+++ server.py
@@ -1,13 +1,1 @@
-from flask import Flask, request
-import os
-
-app = Flask(__name__)
-
-@app.get("/backup")
-def backup():
-    path = request.args["path"]
-    os.system(f"tar czf /tmp/backup.tgz {path}")
-    return "ok"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8079)
+print("Hello, World!")
